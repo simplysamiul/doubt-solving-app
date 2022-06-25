886a0f6b50814b181a4024329ddb20ad1c6f3c1c
Md Samiul <simplysamiul@gmail.com>
2022-06-25 22:03:30 +0600

    create cutome dialoge box

--- src/views/components/teachersRoute/UnResolvedDoubt.js
+++ src/views/components/teachersRoute/UnResolvedDoubt.js
@@ -1,11 +1,20 @@
+import * as React from 'react';
 import '../../../styles/UnResolvedDoubts.css';
+import Dialoge from '../../custome/Dialoage';
+
 const UnResolvedDoubt = ({doubt}) => {
     const {title} = doubt;
-    console.log(doubt);
+    const [open, setOpen] = React.useState(false);
     return (
-        <div className='unresolved-doubt-conatiner'>
+        <div className='unresolved-doubt-area'>
+            <div className="unresolved-doubt-conatiner">
             <h1>{title}</h1>
-            <button>Accept</button>
+            <button variant="outlined" onClick={() => setOpen(true)}> Accept </button>
+            </div>
+            <Dialoge
+            open={open}
+            setOpen={setOpen}
+            />
         </div>
     );
 };
---
+++ src/views/custome/Dialoage.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+
+const Transition = React.forwardRef(function Transition(
+  props, ref,
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function Dialoge({setOpen, open}) {
+
+  return (
+    <>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={() => setOpen(false)}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Let Google help apps determine location. This means sending anonymous
+            location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>Disagree</Button>
+          <Button onClick={() => setOpen(false)}>Agree</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
